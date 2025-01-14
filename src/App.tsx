88fe77f1eda1e5d94c18deae4c750d2e852c37b7
[roddyPiper54] <[hiroyuki.nakajima1887@gmail.com]>
2025-01-15 07:22:31 +0900

    API呼び出し途中

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+//import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0);
+
+  const [threads, SetThreads] = useState();
+
+  //API Thread一覧取得
+  // const threads = () => {
+  fetch("https://railway.bulletinboard.techtrain.dev/")
+    .then((response) => {
+      console.log(response.status);
+
+      if (!response.ok) {
+        console.log("bad");
+      } else {
+        console.log(response);
+        return response;
+      }
+    })
+    .then((json) => {
+      if (json) {
+        SetThreads(json);
+      }
+    });
+  // };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+      <header>
+        <title>
+          <h1>React Bulletion 01</h1>
+        </title>
+      </header>
+      <main>
+        <section>
+          <h1>スレッド一覧</h1>
+          <p></p>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </section>
+      </main>
+      <footer>footer</footer>
+      {/* <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+      </div> */}
+      {/* <div>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </div> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;

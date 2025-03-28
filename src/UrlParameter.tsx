63fb91719efa75551a7cceb647b05476086abd3d
[roddyPiper54] <[hiroyuki.nakajima1887@gmail.com]>
2025-03-28 12:53:27 +0900

    投稿一覧を画面に反映

--- src/UrlParameter.tsx
+++ src/UrlParameter.tsx
@@ -2,38 +2,36 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 export const UrlParameter = () => {
   //スレッド一覧取得
-  const [psts, setPsts] = useState([]);
+  const [threads, setThreads] = useState([]);
   //投稿一覧
-  const [pst, setPst] = useState([]);
-
+  //const [posts, setPost] = useState([]);
   //URLパラメーター
   const { thread_id } = useParams();
 
   //API Thread一覧取得
-  useEffect(() => {
-    //最初に一致した:を""に
-
-    //スレッドの投稿一覧取得
-    //fetchで対象idの投稿一覧取得
-    fetch(`https://railway.bulletinboard.techtrain.dev/threads/${thread_id.replace(/:/, "")}/posts`)
-      .then((response) => {
-        if (!response.ok) {
-          console.log("bad");
-        } else {
-          console.log(psts);
-          return response.json();
-        }
-      })
-      .then((json) => {
-        if (json) {
-          setPsts(json.posts);
-        }
-      })
-      .catch((error) => console.error(error));
-  }, []);
+  //useEffect(() => {
+  //onst getThreads = () => {
+  //fetchで対象idの投稿一覧取得 //最初に一致した:を""に
+  fetch(`https://railway.bulletinboard.techtrain.dev/threads/${thread_id.replace(/:/, "")}/posts`)
+    .then((response) => {
+      if (!response.ok) {
+        console.log("bad");
+      } else {
+        console.log(threads);
+        return response.json();
+      }
+    })
+    .then((json) => {
+      if (json) {
+        setThreads(json.posts);
+        //setPosts(json.posts);
+      }
+    })
+    .catch((error) => console.error(error));
+  //};
+  //}, []);
 
   //スレッド投稿
-
   const postThread = (e) => {
     e.preventDefault();
 
@@ -44,15 +42,14 @@ export const UrlParameter = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ post: pst }), //データをJSON化して送信
+        body: JSON.stringify({ post: posts }), //データをJSON化して送信
       }).then((response) => {
         if (!response.ok) {
           console.log("bad");
         } else {
           console.log("ok");
 
-          window.alert(`${pst}投稿しました。`);
-          //window.location.href = "/";
+          window.alert(`${posts}投稿しました。`);
         }
       });
     } else {
@@ -66,13 +63,13 @@ export const UrlParameter = () => {
         <h1>thread</h1>
         <p>パラメーターは{thread_id}です</p>
         <ul className="threadlist">
-          {psts.map((pst, index) => (
-            <li key={index}>{pst.post}</li>
+          {threads.map((thread, index) => (
+            <li key={index}>{thread.post}</li>
           ))}
         </ul>
 
         <form onSubmit={postThread} className="threadForm">
-          <input type="text" name="" id="" onChange={(e) => setPst(e.target.value)} />
+          <input type="text" name="" id="" onChange={(e) => setPost(e.target.value)} />
           <button type="submit" className="submitBtn">
             投稿
           </button>
